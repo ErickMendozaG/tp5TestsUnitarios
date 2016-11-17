2968d47998baf28687bcaf182bb52d040fba05a8
sanchez-gonzalo94 <sanchez.gonzalo94@gmail.com>
2016-11-17 18:23:02 -0300

    Agregar CU 5 y 16 Gonzalo

---
+++ src/ingsw/NivelUsuario.java
@@ -0,0 +1,15 @@
+package ingsw;
+
+public class NivelUsuario {
+	String nombreNivel;
+	
+	
+
+	public NivelUsuario(String nombreNivel1) {
+		this.nombreNivel = nombreNivel1;
+	}
+	
+	String getNombreNivel(){
+		return nombreNivel;
+	}
+}
